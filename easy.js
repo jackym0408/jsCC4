10b453abea8050362cfdcb9218d0093e1447dec2
Jackeline Mendoza <jackelinemendoza@R2Hs-MBP.lan>
2022-01-31 23:02:27 -0500

    addign first files

---
+++ easy.js
@@ -0,0 +1,25 @@
+// EASY: Write a function that takes in an array of numbers and outputs the average of all the numbers. 
+
+// Example 1:
+
+// Input: [1 , 4 , 7]  Output: 4
+
+// Input: [10, 5]  Output: 7.5
+
+// Input: [1.5, 3, 2.5, 1]  Output: 2
+
+//make a array
+//loop to add all the 
+
+let arr = [1 , 4 , 7];
+
+
+function average(){
+    for(let i = 0; i < arr.length; i++){
+        var sum = lodash.sum(arr);
+
+        
+    }
+
+
+}
